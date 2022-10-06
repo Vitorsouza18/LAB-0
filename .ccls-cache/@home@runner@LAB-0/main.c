9c749a69253650dca7db454df472c69610ca3144
JOAO VITOR <115181669+Vitorsouza18@users.noreply.github.com>
2022-10-06 18:27:51 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@LAB-0/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+  /*Exercicio 1
+  printf("Bem vindo, %s", "Vitor"); */
+  
+  /* Exercio 2
+  int num1 = 0;
+  int num2 = 0;
+  int num3 = 0;
+
+  printf("Informe o primeiro número: \n");
+  scanf("%d", &num1);
+  printf("Informe o segundo número: \n");
+  scanf("%d", &num2);
+  printf("Informe o terceiro número: \n");
+  scanf("%d", &num3);
+
+  int resultado = 0;
+  resultado = (num1 - num2) * num3;
+
+  printf("(%d - %d) * %d = %d", num1, num2, num3, resultado); */
+
+  /*DESAFIO
+  int num = 0;
+  int constante = 2;
+
+  printf("Informe o expoente: \n");
+  scanf("%d", &num);
+
+  int resultado = 0;
+  resultado = pow(constante, num);
+
+  printf("Resultado = %d\n", resultado);*/ 
+  
+  return 0;
+}
